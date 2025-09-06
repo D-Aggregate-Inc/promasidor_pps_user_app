e41ad5cd70817e422b1cea233ea0ba10cac4c7a5
Rasheed Kareem <kareemrasheed89@outlook.com>
2025-09-06 07:11:04 +0100

    updated

--- db/db_utils.py
+++ db/db_utils.py
@@ -88,7 +88,7 @@ def add_user(email, phone, password_hash, role):
         (email, phone, password_hash, role), fetch=None
     )
 def add_location_by_region(region_id,name):
-    execute_query("INSERT INTO location_by_region(region_id,name) VALUES (%s, %s)",(region_id,name), fetch=None)
+    execute_query("INSERT INTO locations_by_region(region_id,name) VALUES (%s, %s)",(region_id,name), fetch=None)
 # Other functions (add_state, add_location, etc.) remain similar
 def add_region(name):
     execute_query("INSERT INTO region (name) VALUES (%s)", (name,), fetch=None)

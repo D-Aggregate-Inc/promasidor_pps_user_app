7c8c5b265c09c6ca8535df79a475c34a8600e91b
Rasheed Kareem <kareemrasheed89@outlook.com>
2025-09-08 11:29:29 +0100

    updated

--- views/merchandiser_onboard.py
+++ views/merchandiser_onboard.py
@@ -57,8 +57,8 @@ with st.expander("Input Outlet Information",expanded=True):
         if image_key:
             if get_outlet_by_phone_contact(phone_contact):
                 st.warning("An outlet with this contact phone already exists.")
-            add_outlet(name, region_name,location_id, classification,outlet_type, user_id, gps_lat, gps_long, image_key,
-                        phone_contact, outlet_number, outlet_address,outlet_landmark, contact_person)
+            add_outlet(name,location_id, classification,outlet_type, user_id, gps_lat, gps_long, image_key,
+                        phone_contact, outlet_number, outlet_address,outlet_landmark, contact_person,region_name)
                     
             st.success("Outlet Onboarded Successfully!")
         else:

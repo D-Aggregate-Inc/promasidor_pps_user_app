4b7fbe13c1e3150f2a44a03bc77c9d749a1de98f
Rasheed Kareem <kareemrasheed89@outlook.com>
2025-09-09 18:49:30 +0100

    updated

--- db/db_utils.py
+++ db/db_utils.py
@@ -118,7 +118,7 @@ def add_outlet(name, location_id, classification,outlet_type, user_id, gps_lat, 
     execute_query(
         """INSERT INTO outlets (name,location_id, classification, outlet_type, onboarded_by_user_id, gps_lat, gps_long, outlet_image_key,phone_contact, outlet_number, outlet_address,  
                    outlet_landmark,contact_person,region,account_no,bank_name,account_name)
-        VALUES (%s, %s, %s,%s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s)""",
+        VALUES (%s, %s, %s,%s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s,%s, %s,%s)""",
         (name, location_id, classification,outlet_type, user_id, gps_lat, gps_long, image_key,phone_contact, outlet_number, outlet_address,  
                    outlet_landmark,contact_person,region,account_no,bank_name,account_name), fetch=None
     )

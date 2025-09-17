cf830fa9b777e4c748086b37add235b3d347d7e9
Rasheed Kareem <kareemrasheed89@outlook.com>
2025-09-17 14:04:05 +0100

    updated

--- db/db_utils.py
+++ db/db_utils.py
@@ -88,7 +88,7 @@ def get_locations_by_user_region(user_id):
         return []
     return execute_query(
         """SELECT l.id, l.name, r.name AS region_name
-           FROM locations l
+           FROM locations_by_region l
            JOIN region r ON l.region_id = r.id
            WHERE r.name = %s""",
         (user['merchandiser_region'],), fetch='all'

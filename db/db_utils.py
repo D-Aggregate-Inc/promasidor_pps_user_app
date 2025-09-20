25dfaa3df2f2c4cf04e8ddbb7ef35965f6040b72
Rasheed Kareem <kareemrasheed89@outlook.com>
2025-09-20 20:43:50 +0100

    updated

--- db/db_utils.py
+++ db/db_utils.py
@@ -216,25 +216,38 @@ def get_seasoning_skus_grouped():
     skus = execute_query("SELECT * FROM skus WHERE name <> 'Onga Chicken-40-60G' AND category ='Seasoning' ORDER BY name")
     grouped={}
     for sku in skus:
-        grouped['category'].append(sku)
-        return grouped
+        cat=sku['category']
+        if cat not in grouped:
+            grouped[cat]=[]
+        grouped[cat].append(sku)
+    return grouped
 def get_diary_general_skus_grouped():
     skus = execute_query("SELECT * FROM skus WHERE category ='Dairy' AND name <> 'Top Tea-200G PP' ORDER BY name")
     grouped ={}
     for sku in skus:
-        grouped['category'].append(sku)
-        return grouped
+        cat=sku['category']
+        if cat not in grouped:
+            grouped[cat]=[]
+        grouped[cat].append(sku)
+    return grouped
 def get_diary_kiosk_skus_grouped():
     skus = execute_query("SELECT * FROM skus WHERE category ='Dairy' AND name <> 'Loya-300Gs' ORDER BY name")
     grouped ={}
     for sku in skus:
-        grouped['category'].append(sku)
-        return grouped
+        cat=sku['category']
+        if cat not in grouped:
+            grouped[cat]=[]
+        grouped[cat].append(sku)
+    return grouped
 def get_diary_tabletop_skus_grouped():
     skus = execute_query("SELECT * FROM skus WHERE category ='Dairy' AND name NOT IN ('Cowbell-300GS','Loya-300Gs','Creamer-Kremela 250-300GS','CB Choco-400g','Miksi-300GS-400GS','Twisco 400-500GS') ORDER BY name")
     grouped ={}
     for sku in skus:
-        grouped['category'].append(sku)
-        return grouped
+        cat=sku['category']
+        if cat not in grouped:
+            grouped[cat]=[]
+        grouped[cat].append(sku)
+    return grouped
+
 def get_posms():
     return execute_query("SELECT * FROM posms")
